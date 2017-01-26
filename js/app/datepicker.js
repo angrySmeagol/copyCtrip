4658bfcf0fe49b9879e8c4384777dcb528f27881
angrysmeagol <zhang_fenguo@sina.com>
2017-01-26 15:15:41 +0800

    group and globle add

--- js/app/datepicker.js
+++ js/app/datepicker.js
@@ -1,51 +1,79 @@
 $(function() {
-	var today=new Date();
-	var tomorrow=new Date(today.valueOf() +86400000);
 	
-	$( "#check-in" ).datepicker({
+	setPicker($("#check-in"),$("#check-out"));
+	setPicker($("#check-in2"),$("#check-out2"));
+	function setPicker(inputIn,inputOut){
+		var today=new Date();
+		var tomorrow=new Date(today.valueOf() +86400000);
+		var inputDom=inputIn;
+		var outputDom=inputOut;
+
+
+		inputDom.datepicker({
 		//beforeShowDay: $.datepicker.noWeekends,
-		//altField: "#check-out",
+		//altField: ".check-out",
 		numberOfMonths: 2,
 		onClose:function(dateText, inst){
-			$("#check-out").datepicker('show');
+			outputDom.datepicker('show');
 		},
 		//appendText: '(yyyy-mm-dd)' ,
 		//buttonText: 'Choose',
 		//changeMonth: true,
 		//changeYear: true
-	});
-	$( "#check-out" ).datepicker({
-		numberOfMonths: 2,
-		beforeShow : function(input){
-			var checkIn=$("#check-in").datepicker( 'getDate' );
-			var checkedOut=$("#check-out").datepicker( 'getDate' );
-			
-			var today=new Date();
-			today.setHours(0);
-			today.setMinutes(0);
-			today.setSeconds(0);
-			today.setMilliseconds(0);
-			var checkOut=new Date(checkIn.valueOf()+86400000);
-			var afterDays=(checkIn-today)/86400000+1;
-			if(checkedOut.valueOf()<=checkIn.valueOf()){
-				$("#check-out").datepicker('setDate',checkOut);
+		});
+		outputDom.datepicker({
+			numberOfMonths: 2,
+			beforeShow : function(input){
+				var checkIn=inputDom.datepicker( 'getDate' );
+				var checkedOut=outputDom.datepicker( 'getDate' );
+				
+				var today=new Date();
+				today.setHours(0);
+				today.setMinutes(0);
+				today.setSeconds(0);
+				today.setMilliseconds(0);
+				var checkOut=new Date(checkIn.valueOf()+86400000);
+				var afterDays=(checkIn-today)/86400000+1;
+				if(checkedOut.valueOf()<=checkIn.valueOf()){
+					outputDom.datepicker('setDate',checkOut);
+				}
+				
+				outputDom.datepicker('option', 'minDate', '+'+afterDays+'d');
+				outputDom.datepicker('option', 'maxDate', '+3w '+'+'+afterDays+'d');
 			}
-			
-			$("#check-out").datepicker('option', 'minDate', '+'+afterDays+'d');
-			$("#check-out").datepicker('option', 'maxDate', '+3w '+'+'+afterDays+'d');
+		});
+		$.datepicker.setDefaults({
+		});
+		inputDom.datepicker('option', 'minDate', '+0d');
+		inputDom.datepicker('option', 'maxDate', '+2m');
+
+		outputDom.datepicker('option', 'minDate', '+1d');
+		outputDom.datepicker('option', 'maxDate', '+3w');
+		inputDom[0].value=today.toLocaleDateString();
+		outputDom[0].value=tomorrow.toLocaleDateString();
+
+	};
+		
+
+////
+	$(".search-block-ul").bind("click",function(e){
+		var target = e.target || e.srcElement;
+		var li=$(".search-block-li");
+		var form=$(".form-block");
+		var index=0;
+		
+		if(target.nodeName.toLowerCase()=="li"){
+			if(target==$(".search-block-li.active")[0]){
+				return;
+			}
+			for(var i=0;i<li.length;i++){
+				if(target==li[i]) index=i;
+			}
+			$(".search-block-li.active").attr("class","search-block-li");
+			$(target).attr("class","search-block-li active");
+			$(".form-block.active").attr("class","form-horizontal form-block");
+			$(form[index]).attr("class","form-horizontal form-block active");
+
 		}
 	});
-	$.datepicker.setDefaults({
-	});
-	$("#check-in").datepicker('option', 'minDate', '+0d');
-	$("#check-in").datepicker('option', 'maxDate', '+2m');
-
-	$("#check-out").datepicker('option', 'minDate', '+1d');
-	$("#check-out").datepicker('option', 'maxDate', '+3w');
-	$(".datepicker")[0].value=today.toLocaleDateString();
-	$(".datepicker")[1].value=tomorrow.toLocaleDateString();
-
-
-	;
-
 });
